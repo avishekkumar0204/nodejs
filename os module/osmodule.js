142aadfbc5b6df947eebe6a4bed3ae2735462300
Avishek <kumaravishek84@gmail.com>
2021-06-20 19:35:13 +0530

    os module

---
+++ os module/osmodule.js
@@ -0,0 +1,12 @@
+const os=require("os");
+const arch=os.arch();
+console.log(arch);
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.type());
+
+const freeRAM=os.freemem();
+console.log(freeRAM/1024/1024/1024);
+
+const totRAM=os.totalmem();
+console.log(totRAM/1024/1024/1024);
