1082afb764733aec7ea0a56eb2535589c0715e7b
Avishek <kumaravishek84@gmail.com>
2021-06-21 00:00:58 +0530

    path module

---
+++ 5.Path module/index.js
@@ -0,0 +1,5 @@
+const path=require("path");
+console.log(path.dirname("E:/nodejs/5.Path module/index.js"));
+console.log(path.extname("E:/nodejs/5.Path module/index.js"));
+console.log(path.basename("E:/nodejs/5.Path module/index.js"));
+console.log(path.parse("E:/nodejs/5.Path module/index.js"));
